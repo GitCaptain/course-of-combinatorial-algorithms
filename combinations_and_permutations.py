9eb706592e67cf2537f8aa702fdcc5b38d248398
Alexander <xbox24_772@mail.ru>
2018-03-01 11:14:20 +0700

    second ex, updated

--- combinations_and_permutations.py
+++ combinations_and_permutations.py
@@ -3,6 +3,11 @@ def comb(n, k):
     A = []
     B = [i+1 for i in range(k)]
 
+    if n == k:
+        return [B]
+    elif n < k:
+        return False
+
     p = k
     while p >= 0:
         A.append(B.copy())
@@ -17,6 +22,11 @@ def comb(n, k):
 def permut(n):
     A = []
     B = [i+1 for i in range(n)]
+
+    if n == 0:
+        return [[0]]
+    elif n < 0:
+        return False
 
     while True:
         A.append(B.copy())
@@ -41,12 +51,12 @@ def permut(n):
 def solve_rhomb():
     from math import sqrt
     get_len_sqr = lambda p1, p2: (p1[0]-p2[0])**2+(p1[1]-p2[1])**2  # квадрат расстояния между точками p1, p2
-    scalar_product = lambda v1, v2: v1[0]*v2[0] + v1[1]*v2[1]  # скалярное произведение?
+    scalar_product = lambda v1, v2: v1[0]*v2[0] + v1[1]*v2[1]  # скалярное произведение
     permutations = permut(4)
 
     def rhomb_square(cur_points):
         for permut in permutations:
-            p1, p2, p3, p4 = [cur_points[permut[i-1]-1] for i in range(4)]
+            p1, p2, p3, p4 = [cur_points[permut[i]-1] for i in range(4)]
             if p1 == p2 == p3 == p4:
                 # вырожденный ромб - не ромб
                 continue
@@ -56,7 +66,7 @@ def solve_rhomb():
             v1 = (p3[0] - p1[0], p3[1] - p1[1])  # диагонали ромба
             v2 = (p2[0] - p4[0], p2[1] - p4[1])
             if scalar_product(v1, v2):
-                # Диагонали должны быть перпендикулярны
+                # Диагонали должны быть перпендикулярны, т.е. скалярное произведение = 0
                 continue
             # Если все условия выполнены, значит ромб, возвращаем его площадь
             return sqrt(get_len_sqr(p1, p3))*sqrt(get_len_sqr(p2, p4))/2
@@ -65,26 +75,27 @@ def solve_rhomb():
 
     best_points = []
     best_square = 0
-    combinations = comb(n, 4)  # все возможные комбинации 4х точек
+    combinations = comb(n, 4)  # все возможные комбинации 4х точек из n
     for c in combinations:  # перебираем текущую комбинацию
         cur_combination = [points[i-1] for i in c]  # точки текущей комбинации
-        cur_square = rhomb_square(cur_combination)
-        if cur_square > best_square:
+        cur_square = rhomb_square(cur_combination)  # площадь текущей комбинации точек
+        if cur_square > best_square:  # если площадь больше текущей максимальной, то предыдущие ромбы не нужны
             best_square = cur_square
             best_points.clear()
-        if cur_square == best_square:
+        if cur_square == best_square:  # если площадь ромба равна максимальной, то добавляем его в лучшие ромбы
             best_points.append(cur_combination)
     return best_square, best_points
 
 
 if __name__ == '__main__':
+
     n = int(input("Введите количество точек и сами точки:\n"))
     points = []
 
     for _ in range(n):
         points.append(tuple(map(int, input().split())))
 
-    rhombs = solve_rhomb()
+    rhombs = solve_rhomb() if n > 3 else (0, 0)
 
     if rhombs[0]:
         print("Максимальная площадь ромба:", rhombs[0])
